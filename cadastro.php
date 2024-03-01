14b76ba61584df4285517d8880a768e653c693e0
roberta <129108044+rxberta@users.noreply.github.com>
2024-03-01 10:58:31 -0300

    código inicial

---
+++ cadastro.php
@@ -0,0 +1,36 @@
+<?php
+
+$login = $_POST['login'];
+$login = MD5($_POST['senha']);
+$connect = mysql_connect('localhost', 'root', '');
+$db = mysql_select_db('login');
+$query_select = "SELECT login FROM usuarios WHERE login = '$login'";
+$select = mysql_query($query_select, $connect);
+$array =  mysql_fetch_array($select);
+$logarray = $array['login'];
+
+if ($login == "" || $login == null){
+    echo"<script language='javascript' type='text/javascript'> alert('O campo login deve ser preenchido');window.location.href='cadastro.html';</script>";
+}
+
+else{
+    if($logarray == $login){
+        echo"<script language='javascript' type='text/javascript'> alert('Esse login já existe');window.location.href='cadastro.html';</script>";
+        die();
+    }
+
+else {
+    $query = "INSERT INTO usuarios (login,senha) VALUES ('$login','$senha')";
+    $insert = mysql_query($query, $connect);
+
+    if($insert){
+        echo"<script language='javascript' type='text/javascript'> alert('Usuário cadastrado com sucesso!');window.location.href='cadastro.html';</script>";
+    }
+
+    else{
+        echo"<script language='javascript' type='text/javascript'> alert('Não foi possivel cadastrar esse usuário');window.location.href='cadastro.html';</script>";
+    }
+}
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+$login_cookie = $COOKIE['login'];
+if(isset($login_cookie)){
+    echo"Bem-Vindo, $login_cookie <br>";
+    echo"Essas informações <font color='green'> PODEM</font> ser acessadas por você";
+}
+
+else{
+    echo"Bem-Vindo, convidado <br>";
+    echo"Essas informações <font color='red'> NÃO PODEM</font> ser acessadas por você";
+    echo"<br><a href'login.html'> Faça Login</a> Para ler o conteúdo";
+}
+
+?>
